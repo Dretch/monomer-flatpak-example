670bca8424308781d10a70b4482841aa5c58ce77
Gareth Daniel Smith <garethdanielsmith@gmail.com>
2023-03-04 14:59:36 +0000

    Use a patched cabal-flatpak.

--- flatpak/generate-manifest.sh
+++ flatpak/generate-manifest.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+WORK_DIR=$(pwd)
 VERSION=$(sed -nr 's/^version:\s*(.*)/\1/p' ../package.yaml)
 
 # create a cabal build plan for the published package
@@ -12,12 +13,23 @@ VERSION=$(sed -nr 's/^version:\s*(.*)/\1/p' ../package.yaml)
  && cd monomer-flatpak-example-$VERSION \
  && cabal new-build --dry-run --disable-tests --disable-benchmarks --with-compiler=ghc-9.2.5)
 
-# add packages from the cabal build plan into our template flatpak manifest
-cabal-flatpak --arch=x86_64 --directory=/tmp/monomer-flatpak-example-$VERSION io.github.Dretch.MonomerFlatpakExample.template.json io.github.Dretch.MonomerFlatpakExample.json
+# add packages from the cabal build plan into our template flatpak manifest, using a
+# patched cabal-flatpak so that alex, happy and c2hs are included in the manifest, and
+# so the fields in the manifest JSON are in a readable order.
+(cd /tmp  \
+ && wget https://hub.darcs.net/Dretch/cabal-flatpak/dist -O cabal-flatpak-patched \
+ && unzip -o cabal-flatpak-patched \
+ && cd cabal-flatpak \
+ && cabal build \
+ && cabal run cabal-flatpak -- \
+  --arch=x86_64 \
+  --build-library-exes \
+  --directory=/tmp/monomer-flatpak-example-$VERSION \
+  $WORK_DIR/io.github.Dretch.MonomerFlatpakExample.template.json \
+  $WORK_DIR/io.github.Dretch.MonomerFlatpakExample.json)
 
-# insert c2hs after language-c, since c2hs needs language-c and nanovg needs c2hs
-# insert desktop file installation at the end, because it needs assets from this app
+# insert desktop file installation at the end, because it needs assets from this app's cabal package
 cat io.github.Dretch.MonomerFlatpakExample.json \
- | jq '.modules |= (map(if type == "object" and .name == "language-c" then [., "extra-libraries/c2hs.json"] else . end) | flatten) + ["install-desktop-files.json"]' \
+ | jq --slurpfile idf install-desktop-files.json '.modules |= . + [$idf[0]]' \
  > io.github.Dretch.MonomerFlatpakExample.json.fixed
 mv io.github.Dretch.MonomerFlatpakExample.json.fixed io.github.Dretch.MonomerFlatpakExample.json
