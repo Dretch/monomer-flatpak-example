aa8321dd86bd8ff46b25717108fee39fa148cedd
Gareth Daniel Smith <garethdanielsmith@gmail.com>
2023-10-14 17:41:47 +0100

    Make compatible with the latest Dretch/cabal-flatpak

--- flatpak/generate-manifest.sh
+++ flatpak/generate-manifest.sh
@@ -24,13 +24,12 @@ source cabal-hacks.sh
   --cabal-install \
   --arch=x86_64 \
   --build-library-exes \
-  --cabal-install-serial \
   --directory=/tmp/monomer-flatpak-example-$VERSION \
   $WORK_DIR/io.github.Dretch.MonomerFlatpakExample.template.yml \
   $WORK_DIR/io.github.Dretch.MonomerFlatpakExample.yml)
 
 # insert hack to workaround cabal bug: https://github.com/haskell/cabal/issues/8923
-head -n 80 io.github.Dretch.MonomerFlatpakExample.yml > io.github.Dretch.MonomerFlatpakExample.yml.fixed
+head -n 82 io.github.Dretch.MonomerFlatpakExample.yml > io.github.Dretch.MonomerFlatpakExample.yml.fixed
 cat <<'EOF1' >> io.github.Dretch.MonomerFlatpakExample.yml.fixed
   - |
     # workaround https://github.com/haskell/cabal/issues/8923 by making pkg-config fail
@@ -45,9 +44,10 @@ cat <<'EOF1' >> io.github.Dretch.MonomerFlatpakExample.yml.fixed
     EOF2
     chmod +x pkg-config-hack/pkg-config
     export PATH=$(pwd)/pkg-config-hack:$PATH
-    cabal --config-file=.cabal/config v1-install -j1 --offline --prefix=/app *.tar.gz
+    cabal --config-file=.cabal/config install -j$FLATPAK_BUILDER_N_JOBS --install-method=symlink --installdir=/app/bin monomer-flatpak-example
+    find /app/cabal/ghc-* -mindepth 1 -not -path '*/monomer-flatpak-example*' -a -not -path '*/incoming' -delete
 EOF1
-tail -n +82 io.github.Dretch.MonomerFlatpakExample.yml >> io.github.Dretch.MonomerFlatpakExample.yml.fixed
+tail -n +87 io.github.Dretch.MonomerFlatpakExample.yml >> io.github.Dretch.MonomerFlatpakExample.yml.fixed
 mv io.github.Dretch.MonomerFlatpakExample.yml.fixed io.github.Dretch.MonomerFlatpakExample.yml
 
 # insert desktop file installation at the end, because it needs assets from this app's cabal package
@@ -55,10 +55,8 @@ cat <<EOF >> io.github.Dretch.MonomerFlatpakExample.yml
 - name: install-desktop-files
   buildsystem: simple
   build-commands:
-  - mkdir -p /app/share/applications
-  - mkdir -p /app/share/icons/hicolor/512x512/apps
-  - mkdir -p /app/share/metainfo
-  - cp /app/share/*/monomer-flatpak-example-*/*.desktop /app/share/applications
-  - cp /app/share/*/monomer-flatpak-example-*/*.png /app/share/icons/hicolor/512x512/apps
-  - cp /app/share/*/monomer-flatpak-example-*/*.metainfo.xml /app/share/metainfo
+  - mkdir -p /app/share/{applications,icons/hicolor/512x512/apps,metainfo}
+  - cp /app/cabal/ghc-*/monomer-flatpak-example-*/share/*.desktop /app/share/applications
+  - cp /app/cabal/ghc-*/monomer-flatpak-example-*/share/*.png /app/share/icons/hicolor/512x512/apps
+  - cp /app/cabal/ghc-*/monomer-flatpak-example-*/share/*.metainfo.xml /app/share/metainfo
 EOF
